4ed7219921b99f4ba5428e1da340ac75c423fce1
Vanishagupta18 <guptavanisha11@gmail.com>
2025-11-19 15:57:13 +0530

    geometry

--- three-JS/src/main.js
+++ three-JS/src/main.js
@@ -3,8 +3,10 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00} );
+// const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+// const geometry = new THREE.SphereGeometry( 1, 10, 10 );
+const geometry = new THREE.CylinderGeometry( 5, 5, 20, 32 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00,wireframe:true} );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
