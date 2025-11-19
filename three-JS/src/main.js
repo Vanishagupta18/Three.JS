ad606dfe32aa156a6c106edac264eee98e29a9a9
Vanishagupta18 <guptavanisha11@gmail.com>
2025-11-19 14:10:08 +0530

    orbitcontrols

--- three-JS/src/main.js
+++ three-JS/src/main.js
@@ -1,10 +1,10 @@
 import * as THREE from 'three';
-
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00} );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
@@ -18,11 +18,13 @@ window.addEventListener( 'resize', () => {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
   } );
-
+const controls = new OrbitControls( camera, renderer.domElement );
+controls.enableDamping = true;
 function animate() {
   window.requestAnimationFrame( animate );
   renderer.render( scene, camera );
   cube.rotation.x += 0.01;
   cube.rotation.y += 0.01;
+  controls.update();
 }
 animate();
